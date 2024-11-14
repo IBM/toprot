505817393846353759ebc84f4ef2bf4de143f849
George Dasoulas <gdasoulas@192.168.2.10>
2024-11-14 14:16:06 +0200

    Cleaned ProtCC representation. Use test_loading.py module to check representation

--- etnn/prot/lifts/registry.py
+++ etnn/prot/lifts/registry.py
@@ -5,8 +5,6 @@ from .residue import residue_lift
 from .ss import ss_lift
 
 LIFTER_REGISTRY = {
-    "atom": atom_lift,
-    "bond": bond_lift,
     "residue": residue_lift,
     "secondary_structure": ss_lift,
     "supercell": supercell_lift,
